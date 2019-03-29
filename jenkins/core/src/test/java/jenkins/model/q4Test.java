dcb58f4520c5df28e3088473dce39d445a89d8d0
Justin Luttrell <47038077+jglu225@users.noreply.github.com>
2019-03-29 15:05:07 -0400

    create test for q4 (attempt two)

---
+++ jenkins/core/src/test/java/jenkins/model/q4Test.java
@@ -0,0 +1,30 @@
+package jenkins.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class idFromFilenameTest {
+	
+	@Test
+	public void testInitialCase() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+	    String filename = "[a-z0-9_. -]+";
+		assertEquals(test.idFromFilename(filename), "[a-z0-9_. -]+");
+		
+	}
+	@Test
+	public void testTilde() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		String filename = "test~this";
+		assertEquals(test.idFromFilename(filename), "testThis");
+		
+	}
+	
+	public void testDollarSign() {
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		String filename = "test$this";
+		assertEquals(test.idFromFilename(filename), "testthis");		
+	}
+
+}
